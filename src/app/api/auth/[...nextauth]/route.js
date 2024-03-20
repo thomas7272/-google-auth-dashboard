37f20230e73488486292d2466352f1b56715feeb
Anton jubin <103031115+thomas7272@users.noreply.github.com>
2024-03-20 15:20:41 +0530

    first commit

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,47 @@
+import connectToDB from "@/database";
+import User from "@/models/user";
+import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google";
+
+const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId:
+        "956141256146-0464qlh72gg6tcr0pue640rporhjserp.apps.googleusercontent.com",
+      clientSecret: "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",
+    }),
+  ],
+  callbacks: {
+    async signIn({ user, account }) {
+      if (account.provider === "google") {
+        const { name, email } = user;
+        try {
+          await connectToDB();
+          const isUserExists = await User.findOne({ email });
+
+          if (!isUserExists) {
+            const res = await fetch("http://localhost:3000/api/user", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({ name, email }),
+            });
+
+            if (res.success) {
+              return user;
+            }
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      }
+
+      return user;
+    },
+  },
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
